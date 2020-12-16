55fd21491052aff2cfe4242d357181a92c67da0f
Juvaria Syeda <sfjuvaria@gmail.com>
2020-12-16 21:45:21 +0000

    find jpg files in nested folder

--- image_quality_2d.py
+++ image_quality_2d.py
@@ -2,22 +2,26 @@
 Created on Wed Dec 16 18:02:45 2020
 @author: JSyeda
 """
-import fnmatch
 import os
-import re
 
 def findfiles(which, where='.'):
-    # '''Returns list of filenames from 'where' path matched by 'which'
-    #    shell pattern. Matching is case-insensitive.'''
-    # TODO: recursive param with walk() filtering
-    rule = re.compile(fnmatch.translate(which), re.IGNORECASE)
-    return [name for name in os.listdir(where) if rule.match(name)]
-# findfiles('*.jpg')
+  file_paths = []
+  for dirpath, dirs, files in os.walk(where):
+    for filename in files: 
+      fname = os.path.join(dirpath,filename) 
+      if fname.lower().endswith(which):
+        file_paths.append(fname)
+  return file_paths
+
 
 if __name__ == '__main__':
 
     # Load data directory folder
-    data_dir = '/home/new/Documents/data_imgs'
- 
-    iff = findfiles( '*.jpg', data_dir)
-  
+    # data_dir = '/home/new/Documents/data_imgs'
+    
+    # Load data directory with nested folders
+    data_dir = '/home/new/Documents/data_cropped/png/CounterfeitAnalysis/2016-2019CAM_CROP1/bch'
+    
+    # Returns list of jpg filepaths in a folder 
+    image_files = findfiles('.jpg', data_dir)
+    print(image_files)
